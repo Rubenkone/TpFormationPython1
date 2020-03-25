520e40dfb99a0a4daf9760540d544aab60b20da0
Rubenkone <62592055+Rubenkone@users.noreply.github.com>
2020-03-25 09:17:44 +0000

    Add files via upload

---
+++ MoyenneArithmetique.py
@@ -0,0 +1,71 @@
+
+import sys #POUR ARRTER LE PROGRAMME A UN NIVEAU BIEN SPECIFIQUE; Puis sys.exit(1) à ajouter au niveau ou l'on veut s'arreter
+
+print("Calcule arithmetique de la moyenne de clase")
+nom=str()
+prenom=str()
+nom=input("Comment vous appelez-vous?  ")
+
+prenom=input("Quel est votre prenom?  ")
+"""déclarer une chaine de caractere avec str (string) et l'afficher
+print(str(nom),nom) ou directement avec print("votre nom est",nom,prenom)"""
+#creer une chaine de caractere vide s=srt()
+N=str(nom) 
+P=str(prenom)
+Vnom=nom.isalpha()
+Vprenom=prenom.isalpha()
+#isapha :booléenne Renvoie True si les caractères dans cette chaîne sont alphabétiques et il y a au moins un caractère
+#Vnom et Vprenom servent a la verification
+#print("votre nom compte",len(N),"caractere")
+#print("votre prenom compte",len(P),"caractere")
+
+                         #Gérer le probleme de caractere à saisir 1/ caractere aphabetique 2/ longueur des caracteres
+
+if  Vnom==True and Vprenom==True :
+	if(len(N)>10 or len(P)>10):
+		print("Le nom et le prenom ne doivent pas exceder 10 caracteres")
+		print("relancez le programme pour réessayer")
+	
+
+	if(len(N)<3 or len(P)<3):
+		print("Le nom et le prenom doivent contenir plus de 3 caracteres")
+		print("relancez le programme pour réessayer")
+	
+	else:
+		print("Bienvenu à vous ",prenom,nom)
+else:
+	print("veuillez entrer que des caracteres ALPHABETIQUE")
+	print("relancez le programme pour réessayer")
+	sys.exit(1)						
+
+							#Calcule arithmetique de la moyenne de 3 notes
+
+note1=input("Entrer votre premiere note"  )
+note2=input("Entrer votre seconde note"  )
+note3=input("Entrer votre troisieme note"  )
+Vnote1=note1.isdigit()
+Vnote2=note2.isdigit()
+Vnote3=note3.isdigit()
+#booléenne Renvoie True si cette chaîne ne contient que des caractères numériques.
+
+if  Vnote1==True and Vnote2==True and Vnote3==True :
+	Total=float(note1)+float(note2)+float(note3)
+	Moyenne=float(Total/3) #gerer les decimal avec un float
+	print("votre moyenne est ",Moyenne)
+else:
+	print("veuillez entrer que des caracteres NUMERIQUE")
+	print("relancez le programme pour réessayer")
+	sys.exit(1)
+
+						#GESTION DES MENTIONS
+
+if (Moyenne>=16):
+	print("tres bon travail ",prenom,nom," Vous avez obtenu la mention TRES BIEN")
+elif(Moyenne>=14 and Moyenne<=16):
+	print("bien travaillé mais vous pouvez faire mieux ",prenom,"Vous avez obtenu la mention BIEN")
+elif(Moyenne>=12 and Moyenne<=14):
+	print("Pas mauvais mais vous devez redoubler d'éffort",prenom,"Vous avez obtenu la mention ASSEZ-BIEN")
+elif(Moyenne>=10 and Moyenne<=12):
+	print("Rien n'est encore perdu, améliorez-vous et n'abandonnez pas",prenom,"Vous avez obtenu la mention PASSABLE")
+else:
+	print("Ce n'est pas bon, vous devez vous remettre auy travail ",prenom,"Vous avez obtenu la mention FAIBLE")
